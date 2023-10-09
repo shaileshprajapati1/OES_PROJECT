b1f0e2e46700e60750526c0350d0d199988f921a
shaileshprajapati1 <shaileshprajapati966@gmail.com>
2023-10-09 21:50:58 +0530

    data

--- project_oes.sql
+++ project_oes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 02, 2023 at 06:25 PM
+-- Generation Time: Oct 09, 2023 at 06:20 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -37,16 +37,18 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `roll_id` int(11) NOT NULL DEFAULT 2,
   `create at` timestamp NULL DEFAULT current_timestamp(),
-  `update at` timestamp NULL DEFAULT current_timestamp()
+  `update at` timestamp NULL DEFAULT current_timestamp(),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `gender`, `email`, `college`, `mob`, `password`, `roll_id`, `create at`, `update at`) VALUES
-(1, 'shailesh', 'Male', 'sh@gmail.com', 'tops', '972468513', '25d55ad283aa400af464c76d713c07ad', 1, '2023-10-02 11:04:10', '2023-10-02 11:04:10'),
-(2, 'ranjeet', 'Male', 'ranjit@gmail.com', 'baou', '8849668513', '25d55ad283aa400af464c76d713c07ad', 2, '2023-10-02 11:05:10', '2023-10-02 11:05:10');
+INSERT INTO `users` (`id`, `name`, `gender`, `email`, `college`, `mob`, `password`, `roll_id`, `create at`, `update at`, `is_deleted`) VALUES
+(1, 'shailesh', 'Male', 'sh@gmail.com', 'baou', '9724668513', '25d55ad283aa400af464c76d713c07ad', 1, '2023-10-09 15:57:13', '2023-10-09 15:57:13', 0),
+(2, 'shiv prajapati', 'Male', 'shiv@gmail.com', 'baou', '8849668513', '25d55ad283aa400af464c76d713c07ad', 2, '2023-10-09 15:58:06', '2023-10-09 15:58:06', 0),
+(3, 'ranjeet singh', 'Male', 'ranjit@gmail.com', 'tops technology', '9724668513', '25d55ad283aa400af464c76d713c07ad', 2, '2023-10-09 15:58:34', '2023-10-09 15:58:34', 0);
 
 --
 -- Indexes for dumped tables
@@ -66,7 +68,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
